4dda82dd9f982a8d3ac339f8c2c88961c1325a0e
Mattapol Kittitham <52232922+mattapol@users.noreply.github.com>
2023-08-27 16:55:47 +0700

    Update all

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const express = require('express'); // framework
+const express = require('express'); // framework สำหรับสร้างเว็บเซิร์ฟเวอร์สำหรับการให้บริการเว็บไซต์บน Node.js
 const chalk = require('chalk'); // ใส่สี
-const debug = require('debug')('app'); // คํําสั่งบนMac DEBUG=express:* node index.js
-const morgan = require('morgan'); // ดู log ว่าคอมเครื่องใดเข้ามาดู
+const debug = require('debug')('app'); // คํําสั่งบนMac DEBUG=express:* node index.js -> ใน package.json ได้ตั้ง scriptsไว้ ให้เขีนนใน Terminal ว่า  npm run debug
+const morgan = require('morgan'); // ดู log ว่ามีเหตุการณ์อะไรเกิดขึ้นจาก request ไหนบ้าง ถ้ามี error โผล่ขึ้นมาก็จะตามหาสาเหตุได้ง่าย
+const path = require('path');
+// nodemon ช่วยให้เวลาเเก้ไขใน js ไม่ต้องกดปิด-เปิด server
 
 const app = express();
-const port = 8000;
+const PORT = process.env.PORT; // ดึงค่า PORT ใน package.json
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/"))); // ดึงไฟล์มาจาก /public/index.html มาเเสดง
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs")
 
 app.get("/", (req,res) => {
-    res.send('Hello brontoDev')
+    res.render('index', {username: 'Abas1999', customers: ["John", "Antonei", "Samual"]});
 })
 
-app.listen(port, ()=>{
-    debug("Listening on port" + chalk.green(" : " + port));
+app.listen(PORT, ()=>{
+    debug("Listening on port" + chalk.green(" : " + PORT));
 })
