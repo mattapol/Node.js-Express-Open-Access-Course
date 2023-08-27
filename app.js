f9fc0d4abf23528f675d05b20aeb9cde6b8ab8a9
Mattapol Kittitham <52232922+mattapol@users.noreply.github.com>
2023-08-27 17:12:23 +0700

    Update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk'); // ใส่สี
 const debug = require('debug')('app'); // คํําสั่งบนMac DEBUG=express:* node index.js -> ใน package.json ได้ตั้ง scriptsไว้ ให้เขีนนใน Terminal ว่า  npm run debug
 const morgan = require('morgan'); // ดู log ว่ามีเหตุการณ์อะไรเกิดขึ้นจาก request ไหนบ้าง ถ้ามี error โผล่ขึ้นมาก็จะตามหาสาเหตุได้ง่าย
 const path = require('path');
+const productRouter = express.Router(); // สร้างลิงค์ให้ไปหน้าเว็บอื่น
 // nodemon ช่วยให้เวลาเเก้ไขใน js ไม่ต้องกดปิด-เปิด server
 
 const app = express();
@@ -12,7 +13,17 @@ app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/"))); // ดึงไฟล์มาจาก /public/index.html มาเเสดง
 
 app.set("views", "./src/views");
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+
+productRouter.route("/").get((req,res) =>{ 
+    res.send("hello world !! I'm Product")
+});
+
+productRouter.route("/1").get((req,res) =>{
+    res.send("hello world !! I'm Product 1")
+});
+
+app.use("/products", productRouter);
 
 app.get("/", (req,res) => {
     res.render('index', {username: 'Abas1999', customers: ["John", "Antonei", "Samual"]});
